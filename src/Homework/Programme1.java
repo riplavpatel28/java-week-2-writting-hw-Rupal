238928bddc4ab2d057f1aad064f20f36c7bffd22
rupalpatel18 <rupalpatel9878@gmail.com>
2023-05-26 15:32:01 +0100

    My homework week 2 written

---
+++ src/Homework/Programme1.java
@@ -0,0 +1,28 @@
+package Homework;
+/**Write a Java programme using the following steps.
+        * 1.1 Declare two instance variables.
+        * 1.2 Declare one instance method.
+        * 1.3 Call both instance variables into the instance method inside the print statement.
+        * 1.4 Declare the Main method.
+        * 1.5 Call the above instance method into the Main method and Run the programme.
+        */
+
+public class Programme1 {
+//1.1Declare two instance variables.
+    int a = 10;
+    int b = 20;
+
+    //1.2Declare one instance method.
+    //1.3 call the both instance variables into the instance method inside the print statement.
+    public void instanceMethod(){
+        System.out.println(a);
+        System.out.println(b);
+    }
+//1.4 Declare the main method.
+public static void main(String[] args) {
+    Programme1 programme1 = new Programme1();
+    programme1.instanceMethod();
+}
+
+
+}
